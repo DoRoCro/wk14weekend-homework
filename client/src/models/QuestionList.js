d877439aa37a88a7e4eb135a6de2822118be607f
Douglas Crooke <douglas.crooke+codeclan@googlemail.com>
2017-05-06 22:58:25 +0100

    define QuestionList class to prompt for guesses

---
+++ client/src/models/QuestionList.js
@@ -0,0 +1,9 @@
+class QuestionList {
+  constructor(q1, q2, q3) {
+    this.question1 = q1
+    this.question2 = q2
+    this.question3 = q3
+  }
+}
+
+module.exports = QuestionList
---
+++ client/src/models/specs/QuestionList_spec.js
@@ -0,0 +1,20 @@
+/* eslint-env mocha */
+var QuestionList = require('../QuestionList')
+var assert = require('assert')
+var questionList
+
+describe('guess who questionList', function () {
+  beforeEach(function () {
+    questionList = new QuestionList('question1', 'question2', 'question3')
+  })
+
+  it('has question1', function() {
+    assert.strictEqual('question1', questionList.question1)
+  })
+  it('has question2', function() {
+    assert.strictEqual('question2', questionList.question2)
+  })
+  it('has question3', function() {
+    assert.strictEqual('question3', questionList.question3)
+  })
+})
